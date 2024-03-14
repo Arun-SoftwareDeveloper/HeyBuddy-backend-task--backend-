bdfe96be4bf4463eca2d957d9927ff4a488b8e5e
Arun-SoftwareDeveloper <arunramasamy46@gmail.com>
2024-03-14 20:43:03 +0530

    modified:   Controllers/UserController.js

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -100,7 +100,7 @@ async function forgotPassword(req, res) {
     <p>We noticed that you requested to reset your password.</p>
     <p>Don't worry! Click the link below to securely reset your password:</p>
      <p>Reset Token:${token}</p>
-    <a href="https://poetic-kleicha-bb5e09.netlify.app/resetPassword" style="background-color: #ff8c00; color: #fff; padding: 10px 15px; text-decoration: none; border-radius: 5px; display: inline-block; font-weight: bold;">Reset Your Password</a>
+    <a href="https://heybuddy-backend-task-frontend.netlify.app/resetPassword" style="background-color: #ff8c00; color: #fff; padding: 10px 15px; text-decoration: none; border-radius: 5px; display: inline-block; font-weight: bold;">Reset Your Password</a>
     <p>If you did not request a password reset, please ignore this email.</p>
   `,
     };
