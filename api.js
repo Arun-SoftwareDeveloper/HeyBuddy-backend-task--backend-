c9592f548a9aefab6c2b6defac41366611a5279b
Arun-SoftwareDeveloper <arunramasamy46@gmail.com>
2024-03-14 12:01:59 +0530

    new file:   .gitignore 	new file:   Controllers/CrudController.js 	new file:   Controllers/UserController.js 	new file:   Models/CrudModel.js 	new file:   Models/UserForm.js 	new file:   Routes/CrudRoutes.js 	new file:   Routes/UserRoutes.js 	new file:   api.js 	new file:   package-lock.json 	new file:   package.json

---
+++ api.js
@@ -0,0 +1,36 @@
+// server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB connection
+const dbUrl =
+  "mongodb+srv://arunramasamy46:arunramasamy46@cluster0.0efmm3f.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+// Routes
+const crudRoutes = require("./Routes/CrudRoutes");
+app.use("/cruds", crudRoutes);
+
+const UserRoutes = require("./Routes/UserRoutes");
+app.use("/", UserRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
